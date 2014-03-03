44b19f63d38d3b63d34a0218855e58db13c2e781
Sean Grove <s@bushi.do>
2014-03-02 22:27:13 -0800

    Make it easy to compare simple ScrollView vs StackPanel performance

--- src/om_stack_panel/core.cljs
+++ src/om_stack_panel/core.cljs
@@ -2,6 +2,7 @@
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
             [om-stack-panel.place-holder :as ph]
+            [om-stack-panel.scroll-view :as sv]
             [om-stack-panel.stack-panel :as sp]))
 
 (enable-console-print!)
@@ -9,30 +10,53 @@
 (def app-state (atom {:text "Hello world!"
                       :items (map ph/rand-place-holder-setting (range 1 501))}))
 
+(def implementation
+  ;;:scroll-view
+  :stack-panel
+  )
+
 (om/root
   (fn [app owner]
     (dom/div nil
      (dom/h1 nil "StackPanel Playground")
-     (dom/div #js {:style #js {:position "absolute"
-                               :top 50
-                               :width "100%"
-                               :height 640}}
-              (om/build sp/stack-panel (om/graft {:scroll-position 0
-                                                  :vp-class        "paginated-activities"
-                                                  :item-com        ph/place-holder
-                                                  :items           (:items app)
-                                                  :item-options    {}}
-                                                 app)
-                        {:opts {;; Initial guess for spacing to minimize pop on
-                                ;; render
-                                :item-height-best-guess 65
-                                ;; Try 1/2/3 for for overdraw in both
-                                ;; direction. Too big will cause large
-                                ;; pauses at the edges. < 1 will show
-                                ;; a smalller window, but has a lot of
-                                ;; rendering glitches
-                                :overdraw-factor 1.5
-                                ;; TODO: Handle resize
-                                :vp-height 640}}))))
+     (if (= implementation :scroll-view)
+       (dom/div nil
+         (dom/h2 nil "Scroll-View (no virtualization)")
+         (dom/div #js {:style #js {:position "absolute"
+                                   :top 50
+                                   :width "100%"
+                                   :height 640}}
+                  (om/build sv/scroll-view
+                            (om/graft {:scroll-position   0
+                                       :vp-class           "paginated-activities"
+                                       :vp-height          640
+                                       :vp-oversized-child 10000
+                                       :render-fn         #(om/build ph/place-holder %)
+                                       :items              (:items app)}
+                                      app))))
+       (dom/div nil
+         (dom/h2 nil "StackPanel")
+         (dom/div #js {:style #js {:position "absolute"
+                                   :top 50
+                                   :width "100%"
+                                   :height 640}}
+                  (om/build sp/stack-panel
+                            (om/graft {:scroll-position 0
+                                       :vp-class        "paginated-activities"
+                                       :item-com        ph/place-holder
+                                       :items           (:items app)
+                                       :item-options    {}}
+                                      app)
+                            {:opts {;; Initial guess for spacing to minimize pop on
+                                    ;; render
+                                    :item-height-best-guess 65
+                                    ;; Try 1/2/3 for for overdraw in both
+                                    ;; direction. Too big will cause large
+                                    ;; pauses at the edges. < 1 will show
+                                    ;; a smalller window, but has a lot of
+                                    ;; rendering glitches
+                                    :overdraw-factor 1.5
+                                    ;; TODO: Handle resize
+                                    :vp-height 640}}))))))
   app-state
   {:target (. js/document (getElementById "app"))})
